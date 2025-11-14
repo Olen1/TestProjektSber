a63b12901f9e9536d09ed36a4e4dd489b5521f9e
Olen1 <alina.xamitova@yandex.ru>
2025-11-14 15:26:15 +0500

    Delete projekt_python directory

--- projekt_python/start_worker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Activating virtual environment...
-call .venv\Scripts\activate.bat
-
-echo Starting Dramatiq worker...
-python -m dramatiq knowledge_platform --processes 1 --threads 4
-
